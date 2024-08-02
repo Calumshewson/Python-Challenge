3f2d9db856a6d1c8d5eebc126018fc719128bb5a
Calumshewson <Calumshewson@gmail.com>
2024-08-02 13:36:45 -0500

    Second Version of PyBank assignment

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,10 +6,11 @@ print("Financial Analysis")
 
 print("---------------------------------------------------------------------------------------------------------------")
 
-#Designate File Path
+#Designate File Paths
 budget_csv = os.path.join(".", "Resources", "budget_data.csv")
+output_file = os.path.join(".", "Analysis", "financial_analysis.txt")
 
-#Initialize staring variable
+#Initialize staring variables
 total_months = 0
 total_sum = 0
 total_change = 0
@@ -54,3 +55,13 @@ print("Total: $",total_sum)
 print("Average Change: $",average_change)
 print("Greatest Increase in Profits: ", increase_month, " ($",greatest_increase, ")")
 print("Greatest Decrease in Profits: ", decrease_month, " ($",greatest_decrease, ")")
+
+# Export the results to a text file
+with open(output_file, "w") as file:
+    file.write("Financial Analysis\n")
+    file.write("---------------------------------------------------------------------------------------------------------------\n")
+    file.write("Total Months: " + str(total_months) + "\n")
+    file.write("Total: $" + str(total_sum) + "\n")
+    file.write("Average Change: $" + str(average_change) + "\n")
+    file.write("Greatest Increase in Profits: " + str(increase_month) + " ($" + str(greatest_increase) + ")\n")
+    file.write("Greatest Decrease in Profits: " + str(decrease_month) + " ($" + str(greatest_decrease) + ")\n")
